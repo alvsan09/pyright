d2d022bb1f5075de544cd9b135fe16f514f7cd62
Philippe Ducas <philippe.ducas@ericsson.com>
2022-06-21 17:44:57 -0400

    housemade greedy search

--- packages/pyright-internal/src/completion/gpt2-predict.py
+++ packages/pyright-internal/src/completion/gpt2-predict.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import torch
+import time
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from nltk.tokenize import RegexpTokenizer
@@ -12,37 +13,38 @@ max_new_tokens = 2
 word_tokens = r'\w+'
 regexp_tokenizer = RegexpTokenizer(word_tokens)
 
-def write_json(any):
-	print(json.dumps(any), flush=True)
+class Model:
 
-def load(model_file_path: str) -> 'tuple[GPT2LMHeadModel, GPT2Tokenizer]':
-	tokenizer = GPT2Tokenizer.from_pretrained(model_file_path, local_files_only=True, padding_side='left')
-	model = GPT2LMHeadModel.from_pretrained(model_file_path, local_files_only=True)
-	return model, tokenizer
+	def __init__(self, model_file_path: str):
+		self.tokenizer: GPT2Tokenizer = GPT2Tokenizer.from_pretrained(model_file_path, local_files_only=True, padding_side='left')
+		self.model: GPT2LMHeadModel = GPT2LMHeadModel.from_pretrained(model_file_path, local_files_only=True)
 
-def predict(model: GPT2LMHeadModel, tokenizer: GPT2Tokenizer, context: 'tuple[str]') -> list :
+	def predict(self, context: 'tuple[str]') -> list :
 
-	# Create key_values recursively for all possible contexts
-	inputs = tokenizer.encode(context)
-	past_key_values = None
-	stack: list[tuple] = []
-	for input in inputs:
-		stack.append((input, past_key_values))
-		outputs = model.forward(torch.tensor([input]), past_key_values=past_key_values, use_cache=True, return_dict=True)
-		past_key_values = outputs.past_key_values
+		inputs = self.tokenizer.encode(context)
+		predictions = self.greedy_search(inputs, 60)
 
-	# Equivalent to the above, but without the for loop
-	inputs = tokenizer.encode(context, return_tensors="pt")
-	outputs = model.forward(inputs, use_cache=True, return_dict=True)
+		# Generate method for greedy search, only 1 sequence
+		inputs = self.tokenizer.encode(context, return_tensors="pt")
+		predictions_classic = self.model.generate(inputs, max_new_tokens=60)
+		
+		return predictions
 
-	# plz work :-(
-	predictions = model.generate(inputs=None, num_return_sequences=max_top_next, num_beams=max_top_next, num_beam_groups=max_top_next,
-	 					diversity_penalty=float(max_top_next + 1), max_new_tokens=max_new_tokens,
-						past=outputs.past_key_values)
-	predictions_classic = model.generate(inputs, num_return_sequences=max_top_next, num_beams=max_top_next, num_beam_groups=max_top_next,
-	 					diversity_penalty=float(max_top_next + 1), max_new_tokens=max_new_tokens)
-	
-	return predictions
+	def greedy_search(self, inputs: 'list[int]', num_new_tokens: int) -> 'list[int]':
+		# Generate previous states recursively
+		# TODO: Cache
+		past_key_values = None
+		for input in inputs:
+			outputs = self.model.forward(torch.tensor([input]), past_key_values=past_key_values, use_cache=True, return_dict=True)
+			past_key_values = outputs.past_key_values
+
+		sequence: 'list[int]' = []
+		while len(sequence) < num_new_tokens :
+			max = outputs.logits.argmax()
+			sequence.append(max.item())
+			outputs = self.model.forward(max.unsqueeze(0), past_key_values=outputs.past_key_values, use_cache=True, return_dict=True)
+		
+		return sequence
 
 def trim(text: str) -> str:
 	words = regexp_tokenizer.tokenize(text)
@@ -50,12 +52,15 @@ def trim(text: str) -> str:
 		return words[0]
 	else:
 		return ""
+		
+def write_json(any):
+	print(json.dumps(any), flush=True)
 
 def main():
 	[pyScriptPath, model_file_path] = sys.argv
 
 	write_json({ "event": "state", "value": "initializing" })
-	model, tokenizer = load(model_file_path)
+	model = Model(model_file_path)
 	write_json({ "event": "state", "value": "ready" })
 
 	while True:
@@ -68,7 +73,7 @@ def main():
 
 		if method == 'predict':
 			context: str = params['context']
-			predictions = predict(model, tokenizer, context)
+			predictions = model.predict(context)
 			# predictions = list(map(trim, predictions))
 			# write_json(predictions)
 			print(predictions)
